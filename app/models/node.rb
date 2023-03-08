081d0ccd6ed5661273245701e62edd325397cc5f
Otavio Coelho <otavio.gmcoelho@gmail.com>
2023-03-08 15:04:42 -0300

    Added

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,2 +1,3 @@
 class Node < ApplicationRecord
+  has_ancestry
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_08_155527) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_08_180002) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_155527) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_nodes_on_ancestry"
   end
 
   create_table "users", force: :cascade do |t|
