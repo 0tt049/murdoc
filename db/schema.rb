056c40d8d222ae668bb866acbb7ff1b9f26ed6b2
victor canedo <victorcanedo1@hotmail.com>
2023-03-08 16:30:08 -0300

    created bookmark model with foreign keys for user and nodes

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_08_183344) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_08_192121) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "node_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["node_id"], name: "index_bookmarks_on_node_id"
+    t.index ["user_id"], name: "index_bookmarks_on_user_id"
+  end
 
   create_table "nodes", force: :cascade do |t|
     t.string "name"
@@ -38,4 +47,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_183344) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookmarks", "nodes"
+  add_foreign_key "bookmarks", "users"
 end
