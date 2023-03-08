eccfe663044d34457d8c5d4067047493fe5db000
victor canedo <victorcanedo1@hotmail.com>
2023-03-08 17:27:21 -0300

    created bookmarks controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :bookmarks, only: %i[create destroy]
+  resources :bookmarks, only: %i[index create destroy]
 end
