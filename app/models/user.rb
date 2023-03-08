d0bbc81e77e06f046b6c6c7a5eaa7d6e70d02488
victor canedo <victorcanedo1@hotmail.com>
2023-03-08 16:31:31 -0300

    -_

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true
   has_many :bookmarks, dependent: :destroy
 end
