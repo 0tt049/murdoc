63383bd1ea7c55495e3d13a50c67a59b19b701ab
Otavio Coelho <otavio.gmcoelho@gmail.com>
2023-03-09 14:05:37 -0300

    done

--- db/migrate/20230309011443_add_ancestry_to_node.rb
+++ db/migrate/20230309011443_add_ancestry_to_node.rb
@@ -1,4 +1,4 @@
-class AddAncestryToNodes < ActiveRecord::Migration[7.0]
+class AddAncestryToNode < ActiveRecord::Migration[7.0]
   def change
     add_column :nodes, :ancestry, :string
     add_index :nodes, :ancestry
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_08_211216) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_011443) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
