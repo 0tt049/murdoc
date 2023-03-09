ffed0d863ae1f61b919a32d8dfaec4393721e21e
Guilherme Muoio <gmmuoio@hotmail.com>
2023-03-09 15:53:23 -0300

    ajax ok

--- app/javascript/controllers/search_nodes_controller.js
+++ app/javascript/controllers/search_nodes_controller.js
@@ -1,9 +1,14 @@
-
 import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="search-movies"
 export default class extends Controller {
   static targets = ["form", "input", "list"]
+
+  connect() {
+    console.log(this.formTarget)
+    console.log(this.inputTarget)
+    console.log(this.listTarget)
+  }
 
   update() {
     const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
