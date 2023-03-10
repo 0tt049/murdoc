0f63d6dbb05b06ee95772b95a31436631fa12a54
victor canedo <victorcanedo1@hotmail.com>
2023-03-10 15:16:51 -0300

    cursor:default; on .logo

--- app/javascript/controllers/search_nodes_controller.js
+++ app/javascript/controllers/search_nodes_controller.js
@@ -4,11 +4,11 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["form", "input", "list"]
 
-  connect() {
-    console.log(this.formTarget)
-    console.log(this.inputTarget)
-    console.log(this.listTarget)
-  }
+  // connect() {
+    // console.log(this.formTarget)
+    // console.log(this.inputTarget)
+    // console.log(this.listTarget)
+  // }
 
   update() {
     const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
