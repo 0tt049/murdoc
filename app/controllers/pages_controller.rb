d2c611f25cde277d7d9636999fc5ef29323219ee
Guilherme Muoio <gmmuoio@hotmail.com>
2023-03-10 11:50:58 -0300

    search com ajax implementado

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,12 +4,19 @@ class PagesController < ApplicationController
   def home
     @nodes = Node.all
     if params[:query].present?
-      @nodes = @nodes.where("name ILIKE ?", "%#{params[:query]}%")
+      @nodes = @nodes.where("lower(name) LIKE :search OR lower(category) LIKE :search OR lower(documentation) LIKE :search", search: "%#{params[:query].downcase}%")
     end
 
     respond_to do |format|
       format.html # Follow regular flow of Rails
       format.text { render partial: "nodes/list", locals: {nodes: @nodes}, formats: [:html] }
     end
+  end
+
+
+  private
+
+  def nodes_params
+    params.require(:node).permit(:query)
   end
 end
