39c716c805401a5e5522406b7dd9c212496bbdad
Guilherme Muoio <gmmuoio@hotmail.com>
2023-03-14 15:57:27 -0300

    search children and siblings

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,8 +7,8 @@ import { application } from "./application"
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
+import NodesAjaxController from "./nodes_ajax_controller"
+application.register("nodes-ajax", NodesAjaxController)
+
 import SearchNodesController from "./search_nodes_controller"
 application.register("search-nodes", SearchNodesController)
-
-import TreeView from "./treeview"
-application.register("treeview", TreeView)
