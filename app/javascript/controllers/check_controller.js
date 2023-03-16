93020c5e557cd08c30f9a60b42ac960695769c6b
victor canedo <victorcanedo1@hotmail.com>
2023-03-16 12:48:17 -0300

    checkbox style

--- app/javascript/controllers/check_controller.js
+++ app/javascript/controllers/check_controller.js
@@ -6,11 +6,33 @@ export default class extends Controller {
   connect() {
   }
 
-  update() {
+  update(event) {
+    // console.log(this.element);
     console.log("update");
     const list = this.listTarget;
-    list.innerHTML = "Hello, Stimulus!"
+    // list.innerHTML = "Hello, Stimulus!"
+
+    // event.preventDefault()
+
+    fetch(this.listTarget.action, {
+      method: "GET",
+      headers: { "Accept": "application/json" },
+      body: new ListData(this.listTarget)
+    })
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data)
+        // list.innerHTML = data.html
+      })
+
+
 
 
   }
 }
+
+// <% if node.present? %>
+//   <% node.siblings.where(category:['instance methods', 'methods']).each do |child| %>
+//     <h2><%= child.name %></h2>
+//   <% end %>
+// <% end %>
