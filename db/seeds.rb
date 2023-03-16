1d242dbf434b0fd9357783035d4041dba112ffac
Otavio Coelho <otavio.gmcoelho@gmail.com>
2023-03-16 18:24:56 -0300

    Added doc to nodes

--- db/seeds.rb
+++ db/seeds.rb
@@ -65,95 +65,20 @@ def make_methods
     end
   end
 end
-# obj = "Class"
-# cat = "class"
 
-# parent = Node.create("name" => obj, "category" => cat)
-# parent.save
-# 3.times do
-#   child = parent.children.create("name" => "Sub#{parent.name}", "category" => cat)
-#   child.save
-# end
-
-# children = Node.find_by(id: 1).children
-
-# children.each do |child|
-#   5.times do
-#     sibling = child.children.create("name" => "Sub#{child.name}", "category" => cat)
-#     sibling.save
-#   end
-# end
-
-# class_list_id = (2..9).to_a
-# 4.times do
-#   my_id = class_list_id.sample
-#   sib_module = Node.find_by(id: my_id).siblings.create("name" => "Module#{my_id}", "category" => "module")
-#   sib_module.save
-#   class_list_id - [my_id]
-# end
-
-# Node.all.each do |n|
-#   5.times do
-#     meth = n.children.create("name" => "method_#{n.name.downcase}", "category" => "instance_method")
-#     meth.save
-#     meth = n.children.create("name" => "method_#{n.name.downcase}", "category" => "method")
-#     meth.save
-#   end
-# end
-
-# Node.all.each do |n|
-#   n.documentation = LOREM
-#   n.save
-# end
-
-
-# def createChild(obj, parent = nil)
-#   unless obj.respond_to?(:name)
-#     return
-#   else
-#     if obj.name == "BasicObject"
-#       node = Node.create("name" => obj.name, "category" => obj.class.name.downcase)
-#     else
-#       node = parent.children.create("name" => obj.name, "category" => obj.class.name.downcase)
-#     end
-#   node.save
-#   node
-#   end
-# end
-
-# def procreate(obj, parent = nil)
-#   if (obj.respond_to?(:name) && obj.name.respond_to?(:to_sym)) && ALLOWED_CONSTANTS.include?(obj.name.to_sym)
-#       parent = createChild(obj, parent)
-#       children = parent.name.constantize.subclasses
-#       children.each { |child| procreate(child, parent) }
-#   end
-# end
-
-# def find_meth(node)
-#   begin
-#     name = node.name.constantize
-#   rescue NameError => e
-#   ensure
-#     unless name.nil?
-#       inst_meth = name.instance_methods(false).sort!
-#       meth = name.methods(false).sort!
-#       inst_meth.each { |m| node.siblings.create("name" => m, "category" => "instance methods") }
-#       meth.each { |m| node.siblings.create("name" => m, "category" => "methods") }
-#     end
-#     node.descendants.each { |sub| find_meth(sub) }
-#   end
-# end
-
-# def put_doc
-#   i = 1
-#   100.times do
-#     node = Node.find_by(id: i)
-#     node.documentation = LOREM
-#     node.save
-#     i += 1
-#   end
-# end
-
+def make_doc
+  node_arr = Node.all
+  node_arr.each do |node|
+    if node.category == "class" || node.category == "module"
+      node.documentation = %x|ri #{node.name}|
+      node.save
+    else
+      parent = node.parent.name
+      node.documentation = %x|ri #{parent}.#{node.name}|
+      node.save
+    end
+  end
+end
 
 ALLOWED_CONSTANTS = [
   "Abbrev",
@@ -318,12 +243,9 @@ ALLOWED_CONSTANTS = [
   "ZeroDivisionError",
   "Zlib"
 ]
-# procreate(BasicObject)
-# root = Node.find_by(id: 1)
-# find_meth(root)
-# put_doc
 
 root = Node.create("name" => BasicObject.name, "category" => "class")
 procreate(root.depth)
 make_modules
 make_methods
+make_doc
